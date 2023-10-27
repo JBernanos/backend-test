e0dc622c8499adaa1aa9282ab85c76a50a626e1e
JBernanos <jp.bf@icloud.com>
2023-10-27 19:44:39 -0300

    fix: validate if favorite_color is registered

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import { validationPipe, assemblyErrorArray } from "../services/validation/validation";
 import { userInputDto } from "../dto/users";
 import saveUser from "../services/users/saveUser";
+import getColors from "../services/colors/getColors";
 
 const router = Router();
 
@@ -10,9 +11,13 @@ router.post("", async (req: Request, res: Response) => {
   const validationErrors = await validationPipe(userInputDto, { ...req.body });
   if (validationErrors.length > 0) res.status(400).send({ message: "Invalid payload.", errors: assemblyErrorArray(validationErrors) });
   else {
-    //TODO: Antes de salvar o cliente, validar a cor favorita, ela deve estar cadastrada na tabela de cores.
-    const { status, data } = await saveUser(req.body);
-    res.status(status).send(data);
+    const registeredColors = await getColors();
+    if (!registeredColors?.includes(req.body.favorite_color)) {
+      res.status(400).send({ message: `Invalid favorite_color, avaliable options are: ${registeredColors}` });
+    } else {
+      const { status, data } = await saveUser(req.body);
+      res.status(status).send(data);
+    }
   }
 });
 
